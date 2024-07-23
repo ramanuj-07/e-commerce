13e8e4f2ef29da18395ed82b1a0adf07441cd4bc
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-07-23 19:53:07 +0530

    Created Pages

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,28 +1,56 @@
-import React from 'react'
-import './Navbar.css';
-import logo from '../assets/Frontend_Assets/logo.png';
-import cart_icon from '../assets/Frontend_Assets/cart_icon.png';
+import React from "react";
+import "./Navbar.css";
+import logo from "../assets/Frontend_Assets/logo.png";
+import cart_icon from "../assets/Frontend_Assets/cart_icon.png";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [menu, setMenu] = useState("shop");
+    
+    
   return (
-      <div className='navbar' >
-          <div className='nav-logo' >
-              <img src={logo} alt="" />
-              <p>SHOPPY</p>
-          </div>
-          <ul className="nav-menu">
-              <li>Shop</li>
-              <li>Men</li>
-              <li>Women </li>
-              <li>Kids</li>
-          </ul>
-          <div className="nav-login-cart">
-              <button>Login</button>
-              <img src={cart_icon} alt="" />
-          </div>
-          
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPY</p>
+      </div>
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          Shop{menu === "shop" ? <hr /> : <></>}{" "}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mens");
+          }}
+        >
+          Men {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("womens");
+          }}
+        >
+          Women {menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          Kids {menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
