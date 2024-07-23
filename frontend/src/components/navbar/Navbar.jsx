97d7225e420a124bc32d0c9972da381469f840a4
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-07-23 13:48:27 +0530

    Update Navbar.jsx

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-import './Navbar.css'
+import './Navbar.css';
+import logo from '../assets/Frontend_Assets/logo.png';
+import cart_icon from '../assets/Frontend_Assets/cart_icon.png';
+
 const Navbar = () => {
   return (
       <div className='navbar' >
+          <div className='nav-logo' >
+              <img src={logo} alt="" />
+              <p>SHOPPY</p>
+          </div>
+          <ul className="nav-menu">
+              <li>Shop</li>
+              <li>Men</li>
+              <li>Women </li>
+              <li>Kids</li>
+          </ul>
+          <div className="nav-login-cart">
+              <button>Login</button>
+              <img src={cart_icon} alt="" />
+          </div>
           
     </div>
   )
