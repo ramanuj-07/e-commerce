cfabbe171d603edfd9643e5640a1bc031107d4dd
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-07-23 20:18:05 +0530

    Added Links to category

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,56 +1,33 @@
-import React from "react";
-import "./Navbar.css";
-import logo from "../assets/Frontend_Assets/logo.png";
-import cart_icon from "../assets/Frontend_Assets/cart_icon.png";
-import { useState } from "react";
+import React from 'react'
+import './Navbar.css';
+import logo from '../assets/Frontend_Assets/logo.png';
+import cart_icon from '../assets/Frontend_Assets/cart_icon.png';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    const [menu, setMenu] = useState("shop");
-    
-    
+    const [menu, setMenu ]  = useState("shop");
   return (
-    <div className="navbar">
-      <div className="nav-logo">
-        <img src={logo} alt="" />
-        <p>SHOPPY</p>
-      </div>
-      <ul className="nav-menu">
-        <li
-          onClick={() => {
-            setMenu("shop");
-          }}
-        >
-          Shop{menu === "shop" ? <hr /> : <></>}{" "}
-        </li>
-        <li
-          onClick={() => {
-            setMenu("mens");
-          }}
-        >
-          Men {menu === "mens" ? <hr /> : <></>}
-        </li>
-        <li
-          onClick={() => {
-            setMenu("womens");
-          }}
-        >
-          Women {menu === "womens" ? <hr /> : <></>}
-        </li>
-        <li
-          onClick={() => {
-            setMenu("kids");
-          }}
-        >
-          Kids {menu === "kids" ? <hr /> : <></>}
-        </li>
-      </ul>
-      <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="" />
-        <div className="nav-cart-count">0</div>
-      </div>
+      <div className='navbar' >
+          <div className='nav-logo' >
+              <img src={logo} alt="" />
+              <p>SHOPPY</p>
+          </div>
+          <ul className="nav-menu">
+              <li onClick={()=>{setMenu("shop")}}><Link to='/' >Shop</Link> {menu==="shop" ?<hr/>:<></>} </li>
+              <li onClick={()=>{setMenu("mens")}}><Link to='/mens'>Men </Link>{menu==="mens" ?<hr/>:<></>}</li>
+              <li onClick={()=>{setMenu("womens")}}><Link to='/womens'> Women</Link>{menu==="womens" ?<hr/>:<></>}</li>
+              <li onClick={()=>{setMenu("kids")}}> <Link to='/kids'>Kids</Link> {menu==="kids" ?<hr/>:<></>}</li>
+          </ul>
+          <div className="nav-login-cart">
+              <Link to='/login'> <button>Login</button></Link>
+              <Link to='/cart' > <img src={cart_icon} alt="" /></Link>
+             
+              <div className="nav-cart-count">0</div>
+          </div>
+          
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
