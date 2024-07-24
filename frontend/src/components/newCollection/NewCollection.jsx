ca9a98f9b1468c984be964f6715afded8391f3d2
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-07-24 23:45:47 +0530

    Created login sign up page

--- frontend/src/components/newCollection/NewCollection.jsx
+++ frontend/src/components/newCollection/NewCollection.jsx
@@ -4,6 +4,7 @@ import new_collection from '../assets/Frontend_Assets/new_collections'
 import Item from '../item/Item'
 
 const NewCollection = () => {
+    
   return (
       <div className='new-collection'>
           <h1>NEW COLLECTION </h1>
@@ -13,7 +14,7 @@ const NewCollection = () => {
                   return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
               })}
           </div>
-          
+
     </div>
   )
 }
--- frontend/src/pages/LoginSignUp.jsx
+++ frontend/src/pages/LoginSignUp.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" name="" id="" placeholder='Your Name' />
+          <input type="email" name="" id="" placeholder='Email Address' />
+          <input type="password" name="" id="" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>
+          Already have an account? <span>Login here</span>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name="" id="" />
+            <p>By Continuing, i agree to the terms of use & privacy policy.</p>
+          </div>
+        </p>
+      </div>
+    </div>
   )
 }
 
