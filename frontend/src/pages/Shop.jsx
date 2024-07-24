e0d71d425e0cbe8bd10f3c28562f166ea0af8255
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-07-24 19:36:52 +0530

    Created New collection

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Hero from '../components/hero/Hero'
 import Popular from '../components/popular/Popular'
 import Offers from '../components/offers/Offers'
+import NewCollection from '../components/newCollection/NewCollection'
 const Shop = () => {
   return (
       <div>
           <Hero />
           <Popular />
           <Offers />
-
+          <NewCollection />
     </div>
   )
 }
