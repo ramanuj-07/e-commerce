c401e8719ab03057cbe9b177b581f093d6d01cee
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-07-24 23:59:51 +0530

    created loginsignup page

--- frontend/src/pages/LoginSignUp.jsx
+++ frontend/src/pages/LoginSignUp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './CSS/LoginSignUp.css'
 
 const LoginSignUp = () => {
   return (
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Product = () => {
+  const { all_product } = useContext(ShopContext);
+  
   return (
-    <div>Product</div>
+    <div>
+
+    </div>
   )
 }
 
